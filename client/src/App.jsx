625e5980de74a942bebe61919958d56522a5e1b9
lequanglocdev <quanglocdev@gmail.com>
2024-04-28 14:35:05 +0700

    setup project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+
+  return (
+    <div className='text-3xl text-red-400'>
+        Đây là client
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const dbConnect = require("./src/config/dbConnect");
+const initRoutes = require("./src/routes");
+const cookieParrser = require("cookie-parser");
+
+const app = express();
+app.use(cookieParrser());
+const port = process.env.PORT || 8888;
+app.use(cors());
+
+// đọc hiểu data mà client gửi lên
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+dbConnect();
+initRoutes(app);
+app.use("/", (req, res) => {
+  res.send("Server success");
+});
+app.listen(port, () => {
+  console.log("Server running on the port:", +port);
+});
